50cc9fe2c33df4fa9ec889f6d961ad66f15608a6
DeynnerASR <rojasdeynner8@gmail.com>
2026-03-18 20:17:28 -0500

    Silver layer terminada

--- scripts/silver.py
+++ scripts/silver.py
@@ -9,9 +9,12 @@ def transform_matches():
     
     df = pd.read_json(files[0])
 
-    df_partidos = df[["match_number","round","group_name","home_team","home_team_code","away_team","away_team_code","home_score","away_score"]]
+    df_partidos = df[["match_number","stadium","stadium_city","stadium_country","round","group_name","home_team","home_team_code","away_team","away_team_code","home_score","away_score"]]
     df_partidos = df_partidos.rename(
         columns={'match_number': 'numero_partido', 
+                 'stadium': 'estadio',
+                 'stadium_city': 'ciudad_estadio',
+                 'stadium_country': 'pais_estadio',
                  'round': 'ronda',
                  'group_name': 'grupo', 
                  'home_team_code': 'codigo_equipo_local', 
@@ -23,7 +26,20 @@ def transform_matches():
                 })
 
     print(df_partidos)
-    #print(df.columns)
     return df_partidos
-    
-transform_matches()
+
+def return_teams():
+    transfromed_matches = transform_matches()
+    df_equipos = transfromed_matches[["codigo_equipo_local", "equipo_local"]].drop_duplicates()
+    print(df_equipos)
+
+
+def return_stadiums():
+    transfromed_matches = transform_matches()
+    df_estadios = transfromed_matches[["estadio", "ciudad_estadio", "pais_estadio"]].drop_duplicates()
+    print(df_estadios)
+
+
+df_teams = return_teams()
+df_stadiums = return_stadiums()
+df_matches = transform_matches()
